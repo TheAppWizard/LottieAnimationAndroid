e10d4af340e83ebcb1ce7e4a3ab9f1fb7688c901
TheAppWizard <70090469+TheAppWizard@users.noreply.github.com>
2020-10-19 21:11:18 +0530

    Initial commit

---
+++ app/src/main/java/com/theappwizard/lottieanimationandroid/MainActivity.java
@@ -0,0 +1,18 @@
+package com.theappwizard.lottieanimationandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+    }
+}
